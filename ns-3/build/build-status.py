cf7f4a4ee96722590b33d9cba96de4f4f7ee0db2
drake7707 <mijnbureauklok@gmail.com>
2016-07-11 20:03:13 +0200

    removed build folder from repo

--- ns-3/build/build-status.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-# Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ahsimulation/ns3-dev-ahsimulation-debug', 'build/scratch/ns3-dev-test1-debug', 'build/scratch/ns3-dev-s1g-mac-test-debug', 'build/scratch/subdir/ns3-dev-subdir-debug', 'build/scratch/ns3-dev-scratch-simulator-debug']
-
-# Scripts that are runnable.
-ns3_runnable_scripts = []
-
