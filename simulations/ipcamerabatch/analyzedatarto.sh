bbef4b99dffd0ea26bf3e40ae98bf8b7a4cb191c
root <root@ns3ah.ns3ah.wall2-ilabt-iminds-be.wall2.ilabt.iminds.be>
2016-08-17 15:44:41 +0200

    Changed batch configuration, added files that were untracked and necessary

---
+++ simulations/ipcamerabatch/analyzedatarto.sh
@@ -0,0 +1,5 @@
+../analyzebatch.pl \
+/proj/wall2-ilabt-iminds-be/ns3ah/ipcamera_rto/ \
+config=name,nsta,ngroup,nrawslotnum,tcpsegmentsize,ipcameradatarate,datamode,minrto \
+stats=edcaqueuelength,tcpconnected,totalnumberofdrops,numberofmactxmissedack,numberoftransmissions,NumberOfDroppedPackets,AveragePacketSentReceiveTime,DropTCPTxBufferExceeded,NumberOfTCPRetransmissions,NumberOfTCPRetransmissionsFromAP,TotalDozeTime,TCPCongestionWindow
+
