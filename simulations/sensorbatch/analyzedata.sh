0c87ba2f965a3fd536d14580a7821dc2526bc080
root <root@ns3ah.ns3ah.wall2-ilabt-iminds-be.wall2.ilabt.iminds.be>
2016-08-18 09:58:16 +0200

    Sensor configuration

---
+++ simulations/sensorbatch/analyzedata.sh
@@ -0,0 +1,5 @@
+../analyzebatch.pl \
+/proj/wall2-ilabt-iminds-be/ns3ah/sensor/ \
+config=name,nsta,ngroup,nrawslotnum,trafficinterval,datamode,apalwaysschedulesfornextslot,contentionperrawslot \
+stats=edcaqueuelength,tcpconnected,totalnumberofdrops,numberofmactxmissedack,numberoftransmissions,NumberOfDroppedPackets,AveragePacketSentReceiveTime,DropTCPTxBufferExceeded,totaldozetime
+
