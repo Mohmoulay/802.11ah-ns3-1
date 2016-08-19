b9216a29573875b7a734a0586a014b81b767fd3b
root <root@ns3ah.ns3ah.wall2-ilabt-iminds-be.wall2.ilabt.iminds.be>
2016-08-19 12:46:46 +0200

    Added bootscript & installation script to git, updated run.sh to host WCF service for distributed simulation processing

--- simulations/ipcamerabatch/run.sh
+++ simulations/ipcamerabatch/run.sh
@@ -1,1 +1,1 @@
- mono --server ../SimulationBuilder.exe common.conf build.conf /proj/wall2-ilabt-iminds-be/ns3ah/ipcamera
+ mono --server ../SimulationBuilder.exe common.conf build.conf /proj/wall2-ilabt-iminds-be/ns3ah/ipcamera --host
