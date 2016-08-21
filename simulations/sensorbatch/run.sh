21ab1715979c2373a81411d3800218d2efb082e5
root <root@ns3ah.ns3ah.wall2-ilabt-iminds-be.wall2.ilabt.iminds.be>
2016-08-21 12:18:09 +0200

    Updated sensor batch script

--- simulations/sensorbatch/run.sh
+++ simulations/sensorbatch/run.sh
@@ -1,1 +1,1 @@
- mono --server ../SimulationBuilder.exe common.conf build.conf /proj/wall2-ilabt-iminds-be/ns3ah/sensor --host
+ mono --server ../SimulationBuilder.exe common.conf build.conf /proj/wall2-ilabt-iminds-be/ns3ah/sensorlarge --host
