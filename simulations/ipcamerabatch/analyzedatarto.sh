1ff41f75cdc7648bcaa189ed8690fabb5fbc0d97
root <root@ns3ah.ns3ah.wall2-ilabt-iminds-be.wall2.ilabt.iminds.be>
2016-08-21 08:39:41 +0200

    Updated scripts on host

--- simulations/ipcamerabatch/analyzedatarto.sh
+++ simulations/ipcamerabatch/analyzedatarto.sh
@@ -1,5 +1,5 @@
 ../analyzebatch.pl \
 /proj/wall2-ilabt-iminds-be/ns3ah/ipcamera_rto/ \
-config=name,nsta,ngroup,nrawslotnum,tcpsegmentsize,ipcameradatarate,datamode,minrto \
-stats=edcaqueuelength,tcpconnected,totalnumberofdrops,numberofmactxmissedack,numberoftransmissions,NumberOfDroppedPackets,AveragePacketSentReceiveTime,DropTCPTxBufferExceeded,NumberOfTCPRetransmissions,NumberOfTCPRetransmissionsFromAP,TotalDozeTime,TCPCongestionWindow
+config=name,nsta,ngroup,nrawslotnum,tcpsegmentsize,ipcameradatarate,datamode,minrto,contentionperrawslot \
+stats=edcaqueuelength,tcpconnected,totalnumberofdrops,numberofmactxmissedack,numberoftransmissions,NumberOfDroppedPackets,AveragePacketSentReceiveTime,DropTCPTxBufferExceeded,NumberOfTCPRetransmissions,NumberOfTCPRetransmissionsFromAP,TotalDozeTime,TCPCongestionWindow,IPCameraSendingRate,IPCameraReceivingRate
 
--- simulations/ipcamerabatch/runrto.sh
+++ simulations/ipcamerabatch/runrto.sh
@@ -1,1 +1,1 @@
- mono --server ../SimulationBuilder.exe common.conf buildrto.conf /proj/wall2-ilabt-iminds-be/ns3ah/ipcamera_rto
+ mono --server ../SimulationBuilder.exe common.conf buildrto.conf /proj/wall2-ilabt-iminds-be/ns3ah/ipcamera_rto --host
